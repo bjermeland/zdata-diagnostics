7726ac183e5f968257a138c9776d66d309c25d89
Markus Bjermeland <markus.bjermeland@zdata.no>
2021-06-23 13:45:25 +0200

    Added server to serve web app and some more visuals

---
+++ server/index.js
@@ -0,0 +1,44 @@
+// Import dependencies
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const path = require('path')
+
+// Create a new express application called 'app'
+const app = express()
+
+// Set our backend port to be either an environment variable or port 5000
+const port = process.env.PORT || 5000
+
+// This application level middleware prints incoming requests to the servers console
+app.use((req, res, next) => {
+    console.log(`Request_Endpoint: ${req.method} ${req.url}`)
+    next()
+})
+
+// Set up the bodyParser middleware
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+
+// Set up the CORS middleware
+app.use(cors())
+
+// This middleware informs the express application to serve our compiled React files
+if (
+    process.env.NODE_ENV === 'production' ||
+    process.env.NODE_ENV === 'staging'
+) {
+    app.use(express.static(path.join(__dirname, 'client/build')))
+
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+    })
+}
+
+// Set our server to listen on the port defiend by our port variable
+app.listen(port, () => console.log(`BACK_END_SERVICE_PORT: ${port}`))
